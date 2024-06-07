d789647eaa4ca53813ed8f91a41d4d6c3e4c56d0
mrnikola01 <vukosavljevicn.va@gmail.com>
2024-06-08 00:48:39 +0200

    Combine two elements tests into one ElementsTest

--- src/test/java/Tests/ElementsTest.java
+++ src/test/java/Tests/ElementsTest.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class AddElementsTest extends BaseTest {
+public class ElementsTest extends BaseTest {
 
     @BeforeMethod
     public void pageSetUp() {
@@ -27,5 +27,24 @@ public class AddElementsTest extends BaseTest {
         for(int i = 0; i < 5; i++) addRemoveElementsPage.clickOnAddElementButton();
 
         Assert.assertEquals(addRemoveElementsPage.getDeleteButtons().size(), 5);
+    }
+
+    @Test
+    public void userCanRemoveElement() {
+        homePage.clickOnLinkText("Add/Remove Elements");
+        addRemoveElementsPage.clickOnAddElementButton();
+        addRemoveElementsPage.clickOnAllDeleteButtons();
+
+        Assert.assertEquals(addRemoveElementsPage.getDeleteButtons().size(), 0);
+
+    }
+
+    @Test
+    public void userCanRemoveMultipleElements() {
+        homePage.clickOnLinkText("Add/Remove Elements");
+        for(int i = 0; i < 5; i++) addRemoveElementsPage.clickOnAddElementButton();
+        for(int i = 0; i < 5; i++) addRemoveElementsPage.clickOnAllDeleteButtons();
+
+        Assert.assertEquals(addRemoveElementsPage.getDeleteButtons().size(), 0);
     }
 }
